2b847aa39db097313078b25a372fd3e26d7f9625
Juancho432 <juanmanueljaramillojaramillo4@gmail.com>
2023-09-10 12:31:25 -0500

    apt Instalation File

---
+++ apt_setup.sh
@@ -0,0 +1,18 @@
+sudo apt update
+sudo apt install zsh fuse
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+mkdir ~/.config
+mkdir ~/.fonts
+mv .config/* ~/.config/
+mv .fonts/* ~/.fonts
+mv .p10k.zsh ~
+mv .zshrc ~
+wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+sudo mv nvim.appimage /bin/nvim
+sudo chmod 777 /bin/nvim
+
